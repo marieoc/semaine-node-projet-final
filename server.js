149900ad8b87a519765d825ed806dc2f37e0e181
Marie <marie.occelli13@gmail.com>
2024-03-07 14:52:45 +0100

    add router

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './router/router.js';
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', router);
 
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`)
