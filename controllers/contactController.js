b4b22d6d15d08f39b8501e0208180723e372a5a2
Marie <marie.occelli13@gmail.com>
2024-03-08 09:46:17 +0100

    modify-contact form

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -29,7 +29,9 @@ const contactController = {
   },
 
   create: async (req, res) => {
-    res.render("contact-form");
+    let contact = null; // pass contact variable to view so form can either be used for creating or updating
+
+    res.render("contact-form", { contact });
   },
 
   store: async (req, res) => {
@@ -49,6 +51,68 @@ const contactController = {
     // Update csv file
     fs.appendFileSync(dataFile, `\n${stringifiedData}`, (err) => {
       if (err) throw err;
+      console.log("file updated");
+    });
+
+    res.redirect("/");
+  },
+
+  edit: async (req, res) => {
+    // Retrieve params
+    const { id } = req.params;
+
+    // Retrieve contacts
+    const data = await fs.promises.readFile(dataFile, "utf8");
+    const contacts = parse(data, { delimiter: "," });
+
+    // Find contact
+    let contact = contacts.find((el) => id == el[0]);
+
+    // Format data to give it properties
+    contact = {
+      id: contact[0],
+      civility: contact[1],
+      lastName: contact[2],
+      firstName: contact[3],
+      phone: contact[4],
+      email: contact[5],
+    };
+
+    res.render("contact-form", { contact });
+  },
+
+  update: async (req, res) => {
+    // Retrieve contact id
+    const { id } = req.params;
+    // Retrieve form input values
+    const { civility, firstName, lastName, phone, email } = req.body;
+
+    // create new data arr
+    const updatedContact = [id, civility, lastName, firstName, phone, email];
+
+    // Find contact in csv file
+    fs.readFile(dataFile, "utf8", async (err, data) => {
+      if (err) throw err;
+
+      const contacts = parse(data, { delimiter: "," });
+      console.log(contacts);
+
+      const foundContactIndex = contacts.findIndex(
+        (contact) => contact[0] == id
+      );
+
+      if (foundContactIndex !== -1) {
+        // update existingContact with data from updatedContact
+        contacts[foundContactIndex] = updatedContact;
+
+        // add "," between each to contact array el, then add line space between each contact of array
+        let updatedData = contacts
+          .map((contact) => contact.join(","))
+          .join("\n");
+
+        // Update file
+        await fs.promises.writeFile(dataFile, updatedData, "utf8");
+      }
       console.log("file updated");
     });
 
--- router/router.js
+++ router/router.js
@@ -11,4 +11,7 @@ router.get('/contact/:id', contactController.index);
 router.get('/add-contact', contactController.create);
 router.post('/add-contact', contactController.store);
 
+router.get('/modify-contact/:id', contactController.edit);
+router.post('/modify-contact/:id', contactController.update);
+
 export default router;
